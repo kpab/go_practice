e2b8352d0816f27f0d799acffc7fa8b0297c1aab
kpab <91237200+kpab@users.noreply.github.com>
2024-10-25 21:52:41 +0900

    Update main.go

--- main.go
+++ main.go
@@ -3,29 +3,11 @@ package main
 import "fmt"
 
 // 型
-// 浮動小数点型
+// 論理値型
 
 func main(){
-	var fl64 float64 = 2.4
-	fmt.Println(fl64)
+	var t, f bool = true, false
 
-	fl := 1.6
-	fmt.Println(fl + fl64)
-	fmt.Printf("%T, %T\n", fl64, fl)
-
-	var fl32 float32 = 1.2
-	fmt.Printf("%T\n", fl32)
-
-	fmt.Printf("%T\n", float64(fl32))
-
-	zero := 0.0
-	pinf := 1.0/zero
-	fmt.Println(pinf)
-
-	ninf := -1.0/zero
-	fmt.Println(ninf)
-
-	nan := zero/zero
-	fmt.Println(nan)
+	fmt.Println(t, f)
 	
 }
