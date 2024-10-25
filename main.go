b48b31499e3954610a104db5ed1aa3ef8fca37ed
kpab <91237200+kpab@users.noreply.github.com>
2024-10-25 19:27:24 +0900

    Update main.go

--- main.go
+++ main.go
@@ -4,28 +4,27 @@ import (
 	"fmt"
 )
 
+func outer(){
+	var s4 string = "outer"
+	fmt.Println(s4)
+}
+
 func main(){
 	// 明示的な定義
 	// var 変数名 型 = 値
 	var i int = 100
 	fmt.Println(i)
 
-	var s string = "Hello Go"
-	fmt.Println(s)
+	//  暗黙的な定義
+	// 変数名 := 値
+	i2 := 200
+	fmt.Println(i2)
 
-	var t, f bool = true, false
-	fmt.Println(t, f)
+	i2 = 250
+	fmt.Println(i2)
 
-	var (
-		i2 int = 3
-		s2 string = "Hello World"
-	)
-	fmt.Println(i2, s2)
-	var i3 int
-	var s3 string
-	i3 = 300
-	s3 = "Let's Gooooo"
-	fmt.Println(i3, s3)
-	i = 150
-	fmt.Println(i)
+	outer()
+
+	var s5 string = "Not Use"
+	fmt.Println(s5)
 }
