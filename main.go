2418305b449332f85ef29ff748db123e7f375d3a
kpab <91237200+kpab@users.noreply.github.com>
2024-10-25 19:18:25 +0900

    Update main.go

--- main.go
+++ main.go
@@ -21,4 +21,11 @@ func main(){
 		s2 string = "Hello World"
 	)
 	fmt.Println(i2, s2)
+	var i3 int
+	var s3 string
+	i3 = 300
+	s3 = "Let's Gooooo"
+	fmt.Println(i3, s3)
+	i = 150
+	fmt.Println(i)
 }
