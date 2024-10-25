f361c594595659fc4edd2b3c15838508fd0d73e0
kpab <91237200+kpab@users.noreply.github.com>
2024-10-25 19:13:51 +0900

    Update main.go

--- main.go
+++ main.go
@@ -2,10 +2,23 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
 func main(){
-	fmt.Println("Hello World")
-	fmt.Println(time.Now())
+	// 明示的な定義
+	// var 変数名 型 = 値
+	var i int = 100
+	fmt.Println(i)
+
+	var s string = "Hello Go"
+	fmt.Println(s)
+
+	var t, f bool = true, false
+	fmt.Println(t, f)
+
+	var (
+		i2 int = 3
+		s2 string = "Hello World"
+	)
+	fmt.Println(i2, s2)
 }
