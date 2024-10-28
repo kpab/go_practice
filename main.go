2e31bb0c5ed57cd99fb7983d48dfbe5f2bf661c3
kpab <91237200+kpab@users.noreply.github.com>
2024-10-28 17:19:39 +0900

    Update main.go
    関数

--- main.go
+++ main.go
@@ -2,16 +2,40 @@ package main
 
 import "fmt"
 
-// 論理演算子
+// 関数
+
+func Plus(x, y int) int {
+	return x + y
+}
+
+func Div(x,y int)(int, int){
+	q := x/y
+	r := x%y
+	return q, r
+}
+
+// 返り値の変数も指定可
+func Double(price int)(result int){
+	result = price * 2
+	return
+}
+
+func Noreturn(){
+	fmt.Println("No return")
+}
 
 func main(){
-	fmt.Println(true && false == true)
-	fmt.Println(true && true == true)
-	fmt.Println(true && false == false)
+	i:= Plus(2, 5)
+	fmt.Println(i)
 
-	fmt.Println(true || false == true)
-	fmt.Println(false || false == true)
+	i2, i3 := Div(9, 3)
+	fmt.Println(i2, i3)
 
-	fmt.Println(!true)
-	fmt.Println(!false)
+	i4, _ := Div(5, 2)
+	fmt.Println(i4)
+
+	i5 := Double(1000)
+	fmt.Println(i5)
+
+	Noreturn()
 }
