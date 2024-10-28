cda7bfc61467469d2147f40a22914859eeaea79c
kpab <91237200+kpab@users.noreply.github.com>
2024-10-28 16:21:02 +0900

    Update main.go
    interface型

--- main.go
+++ main.go
@@ -2,35 +2,21 @@ package main
 
 import "fmt"
 
-// 型
-// 配列型
-// あとから要素数を変更できない
+// 型 & nil
+// interface型
+// すべての型と互換性がある
+// nil はpythonでいうnone
+
 func main(){
-	// 明示的
-	var arr1 [3]int
-	fmt.Println(arr1)
-	fmt.Printf("%T\n", arr1)
+	var x interface{}
+	fmt.Println(x)
 
-	var arr2 [3]string =  [3]string{"A", "B", "C"}
-	fmt.Println(arr2)
+	x = 1
+	fmt.Println(x)
+	fmt.Printf("%T\n", x)
 
-	// 暗示的
-	arr3 := [3]int{1, 2, 3}
-	fmt.Println(arr3)
-
-	// 要素数省略
-	arr4 := [...]string{"D", "E"}
-	fmt.Println(arr4)
-	fmt.Printf("%T\n", arr4)	
-
-	// 要素数の操作
-	fmt.Println(arr2[0])
-	fmt.Println(arr2[2])
-	arr2[2] = "X"
-	fmt.Println(arr2[2])
-
-	// var arr5 [4]int
-	// arr5[5]= arr1
-	// fmt.Println(arr5)
-	fmt.Println(len(arr1))
+	x = 2
+	// 更新はできるが、計算演算はできない
+	// fmt.Println(x+3)
+	
 }
