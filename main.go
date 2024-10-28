08249e119ade1ac5691b22e8d9c132eb5d34dc62
kpab <91237200+kpab@users.noreply.github.com>
2024-10-28 22:19:02 +0900

    Update main.go
    型スイッチ

--- main.go
+++ main.go
@@ -5,53 +5,59 @@ import (
 )
 
 // switch
-// 式スイッチ
+// 型スイッチ
+
+
+func anything(a interface{}){
+	// fmt.Println(a)
+	switch v := a.(type) {
+	case string:
+		fmt.Println(v + "!?")
+	case int:
+		fmt.Println(v + 10000)
+	}
+}
 
 func main(){
-	/*
-	n := 1
-	switch n {
-	case 1, 2:
-		fmt.Println("1 or 2")
-	case 3, 4:
-		fmt.Println("3 or 4")
-	default:
+	anything("aaa")
+	anything(1)
+
+	// 型アサーション
+	var x interface{} = 3
+	i, isInt := x.(int) // 復元
+	fmt.Println(i + 2, isInt)
+	// fmt.Println(x + 2)
+
+	f, isFloat64 := x.(float64)
+	fmt.Println(f, isFloat64)
+
+	if x == nil {
+		fmt.Println("None")
+	} else if i, isInt := x.(int); isInt {
+		fmt.Println(i, "x is int")
+	} else if s, isStr := x.(string); isStr {
+		fmt.Println(s, "x is string")
+	} else {
 		fmt.Println("I don't know")
 	}
-	*/
 
-	/*
-	switch n:= 2; n {
-	case 1, 2:
-		fmt.Println("1 or 2")
-	case 3, 4:
-		fmt.Println("3 or 4")
+	switch x.(type) {
+	case int:
+		fmt.Println("int")
+	case string:
+		fmt.Println("string")
 	default:
-		fmt.Println("I don't know")
+		fmt.Println("I don't know")		
 	}
-	*/
 
-	/*
-	n := 6
-	switch {
-	case n > 0 && n < 4:
-		fmt.Println("0yoriookiku4yoritiisai")
-	case n > 3 && n < 7:
-		fmt.Println("nは3より大きく7より小さい")
-	}
-	*/
-
-	switch n:= 2; n {
-	case 1, 2:
-		fmt.Println("1 or 2")
-	case 3, 4:
-		fmt.Println("3 or 4")
-		/*
-	case n > 3 && n < 6:
-		fmt.Println("nは3より大きく6より小さい")
-		*/
-	default:
-		fmt.Println("I don't know")
+	switch v := x.(type) {
+	case bool:
+		fmt.Println(v, "bool")
+	case int:
+		fmt.Println(v, "int")
+	case string:
+		fmt.Println(v, "string")
 	}
 
 }
+
