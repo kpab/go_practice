3467bd12b267d940f5208df6007a46a2559b1887
kpab <91237200+kpab@users.noreply.github.com>
2024-10-28 21:24:40 +0900

    Update main.go
    ジェネレーターの実装

--- main.go
+++ main.go
@@ -2,22 +2,26 @@ package main
 
 import "fmt"
 
-// クロージャーの実装
-// クロージャーとは: 関数と関数の処理に関する関数外の環境をセットとして閉じ込めたもの
-func Later() func(string) string {
-	var store string
-	return func(next string) string{
-		s := store
-		store = next
-		return s
+// ジェネレーターの実装
+
+func integers() func() int {
+	i := 0
+	return func() int {
+		i++
+		return i
 	}
+
 }
 
 func main(){
-	f := Later()
-	fmt.Println(f("Hello"))
-	fmt.Println(f("My"))
-	fmt.Println(f("Name"))
-	fmt.Println(f("is"))
-	fmt.Println(f("golang"))
+	ints := integers()
+	fmt.Println(ints())
+	fmt.Println(ints())
+	fmt.Println(ints())
+	fmt.Println(ints())
+
+	otherints := integers()
+	fmt.Println(otherints())
+	fmt.Println(otherints())
+	fmt.Println(otherints())
 }
