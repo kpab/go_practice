95dd343c38069c052bb90a7a9cc36e4b00d99ac7
kpab <91237200+kpab@users.noreply.github.com>
2024-10-28 22:44:37 +0900

    Update main.go
    panic & recover

--- main.go
+++ main.go
@@ -1,43 +1,18 @@
 package main
 
-import (
-	"fmt"
-	"os"
-)
+import "fmt"
 
-// defer(デファー): 関数終了時に行いたい処理を指定
-
-func TestDefer() {
-	defer fmt.Println("END")
-	fmt.Println("START")
-}
-// 複数ある場合は下から実行
-func RunDefer(){
-	defer fmt.Println("1")
-	defer fmt.Println("2")
-	defer fmt.Println("3")
-}
+// panic & recover
+// 例外処理(pgmを強制終了させる)
 
 func main(){
-	TestDefer()
-
-	/*
-	defer func() {
-		fmt.Println("1")
-		fmt.Println("2")
-		fmt.Println("3")
+	defer func(){
+		if x := recover(); x != nil {
+			fmt.Println(x)
+		}
 	}()
-	*/
-
-	RunDefer()
-
-	// リソースの解放処理
-	file, err := os.Create("test.txt")
-	if err != nil {
-		fmt.Println(err)
-	}
-	defer file.Close()
-
-	file.Write([]byte("Hello"))
+	
+	panic("runtime error")
+	fmt.Println("START")
 }
 
