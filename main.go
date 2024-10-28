74d0f485ec140d42e7278f7f2d15461fb167ead7
kpab <91237200+kpab@users.noreply.github.com>
2024-10-28 22:29:49 +0900

    Update main.go
    ラベル付きfor

--- main.go
+++ main.go
@@ -4,60 +4,32 @@ import (
 	"fmt"
 )
 
-// switch
-// 型スイッチ
-
-
-func anything(a interface{}){
-	// fmt.Println(a)
-	switch v := a.(type) {
-	case string:
-		fmt.Println(v + "!?")
-	case int:
-		fmt.Println(v + 10000)
-	}
-}
+// ラベル付きfor
 
 func main(){
-	anything("aaa")
-	anything(1)
-
-	// 型アサーション
-	var x interface{} = 3
-	i, isInt := x.(int) // 復元
-	fmt.Println(i + 2, isInt)
-	// fmt.Println(x + 2)
-
-	f, isFloat64 := x.(float64)
-	fmt.Println(f, isFloat64)
-
-	if x == nil {
-		fmt.Println("None")
-	} else if i, isInt := x.(int); isInt {
-		fmt.Println(i, "x is int")
-	} else if s, isStr := x.(string); isStr {
-		fmt.Println(s, "x is string")
-	} else {
-		fmt.Println("I don't know")
+/*
+Loop:
+	for {
+		for {
+			for {
+				fmt.Println("START")
+				break Loop
+			}
+			fmt.Println("処理をしない")
+		}
+		fmt.Println("処理をしない")
 	}
-
-	switch x.(type) {
-	case int:
-		fmt.Println("int")
-	case string:
-		fmt.Println("string")
-	default:
-		fmt.Println("I don't know")		
+	fmt.Println("END")
+*/
+	Loop:
+	for i := 0; i < 3; i++ {
+		for j := 1; j < 3; j++ {
+			if j > 1 {
+				continue Loop
+			}
+			fmt.Println(i, j, i * j)
+		}
+		fmt.Println("処理をしない")
 	}
-
-	switch v := x.(type) {
-	case bool:
-		fmt.Println(v, "bool")
-	case int:
-		fmt.Println(v, "int")
-	case string:
-		fmt.Println(v, "string")
-	}
-
 }
 
