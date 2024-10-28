ef526bb773a9a642707d49f57f46e53ae41f6299
kpab <91237200+kpab@users.noreply.github.com>
2024-10-28 16:55:16 +0900

    Update main.go
    算術演算子

--- main.go
+++ main.go
@@ -2,44 +2,34 @@ package main
 
 import "fmt"
 
-// 定数
-// 頭文字を大文字にすると他から呼び出せる
-const Pi = 3.14
-// 複数定義
-const (
-	URL = "https://google.com"
-	SiteName = "google"
-)
+// 算術演算子
 
-// 値の省略
-const (
-	A = 1
-	B
-	C
-	D = "A"
-	E
-	F
-)
 
-// 巨大な数値の定義が可能
-// var Big int = 9223372036854775807 + 1
-const Big = 9223372036854775807 + 1
 
-// iota
-const (
-	c0 = iota// 連続する整数の連番を生成する
-	c1
-	c2
-)
 
 func main(){
-	fmt.Println(Pi)
-	// Pi = 3 // 定数は上書きできない
-	fmt.Println(URL, SiteName)
+	fmt.Println(1 + 2)
+	fmt.Println("ABC" + "DE") // 文字列の結合もできるよん
 
-	fmt.Println(A, B, C, D, E, F)
+	fmt.Println(5 - 1)
 
-	fmt.Println(Big-1)
+	fmt.Println(5 * 4)
 
-	fmt.Println(c0, c1, c2)
+	fmt.Println(60 / 4)
+
+	fmt.Println(9 % 4) // 余り
+
+	// 算術演算子と代入
+	n := 0
+	n += 4
+	fmt.Println(n)
+	n++ // n = n + 1
+	fmt.Println(n)
+	n-- // n = n-1
+	fmt.Println(n)
+
+	s:="ABC"
+	s+="DE"
+	fmt.Println(s)
+
 }
