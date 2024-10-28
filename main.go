bfd0a4bb063ebeb9972c424875c4ae65570b0f33
kpab <91237200+kpab@users.noreply.github.com>
2024-10-28 17:59:21 +0900

    Update main.go
    無名関数

--- main.go
+++ main.go
@@ -2,40 +2,19 @@ package main
 
 import "fmt"
 
-// 関数
+// 無名関数
 
-func Plus(x, y int) int {
-	return x + y
-}
-
-func Div(x,y int)(int, int){
-	q := x/y
-	r := x%y
-	return q, r
-}
-
-// 返り値の変数も指定可
-func Double(price int)(result int){
-	result = price * 2
-	return
-}
-
-func Noreturn(){
-	fmt.Println("No return")
-}
 
 func main(){
-	i:= Plus(2, 5)
+	f := func(x, y int)int{
+		return x+y
+	}
+
+	i := f(1, 2)
 	fmt.Println(i)
 
-	i2, i3 := Div(9, 3)
-	fmt.Println(i2, i3)
-
-	i4, _ := Div(5, 2)
-	fmt.Println(i4)
-
-	i5 := Double(1000)
-	fmt.Println(i5)
-
-	Noreturn()
+	i2 := func(x, y int)int{
+		return x+y
+	}(1, 2)
+	fmt.Println(i2)
 }
