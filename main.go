2636b78d1b5e2a2558fa3325722148afd1d44277
kpab <91237200+kpab@users.noreply.github.com>
2024-10-28 18:04:55 +0900

    Update main.go
    関数を引数にとる関数

--- main.go
+++ main.go
@@ -2,15 +2,15 @@ package main
 
 import "fmt"
 
-// 関数を返す関数
-
-func ReturnFunc()func(){
-	return func(){
-		fmt.Println("I'm a function")
-	}
+// 関数を引数にとる関数
+func CallFunction(f func()){
+	f()
 }
 
+
+
 func main(){
-	f := ReturnFunc()
-	f()
+	CallFunction(func(){
+		fmt.Println("I'm a function")
+	})	
 }
