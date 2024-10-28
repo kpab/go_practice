c878d4487facd8bc987ff68bc8a119e29b708e72
kpab <91237200+kpab@users.noreply.github.com>
2024-10-28 16:36:41 +0900

    Update main.go
    型変換

--- main.go
+++ main.go
@@ -1,22 +1,53 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-// 型 & nil
-// interface型
-// すべての型と互換性がある
-// nil はpythonでいうnone
+// 型変換
 
 func main(){
-	var x interface{}
-	fmt.Println(x)
+	/*
+	var i int = 1
+	fl64 := float64(i)
+	fmt. Println(fl64)
+	fmt.Printf("i = %T\n", i)
+	fmt.Printf("fl64 = %T\n", fl64)
+	i2 := int(fl64)
+	fmt.Println(i2)
+	fmt.Printf("%T\n", i2)
 
-	x = 1
-	fmt.Println(x)
-	fmt.Printf("%T\n", x)
+	fl := 10.5
+	i3 := int(fl)
+	fmt.Printf("fl = %T\n", fl)
+	fmt.Println(fl)
+	fmt.Printf("i3 = %T\n", i3)
+	fmt.Println(i3)
+	// float → int: 小数点以下は切り捨て
 
-	x = 2
-	// 更新はできるが、計算演算はできない
-	// fmt.Println(x+3)
-	
+	*/
+	// string to int
+	/*
+	var s string = "100"
+	fmt.Printf("s = %T\n", s)
+	i, _ := strconv.Atoi(s) // 二つ目の値を破棄
+	fmt.Println(i)
+	fmt.Printf("i = %T\n", i)
+	*/
+	// int to string
+	/*
+	var i2 = 200
+	s2 := strconv.Itoa(i2)
+	fmt.Println(s2)
+	fmt.Printf("%T\n", s2)
+	*/
+	// string to byte
+	var h string = "Hello World"
+	b := []byte(h)
+	fmt.Println(b)
+	fmt.Printf("%T\n", b)
+
+	// byte to string
+	h2 := string(b)
+	fmt.Println(h2)
 }
