0d0b62b50ab82e0a6c66b897e695f7b4ac29678f
kpab <91237200+kpab@users.noreply.github.com>
2024-10-28 17:04:56 +0900

    Update main.go
    論理演算子

--- main.go
+++ main.go
@@ -2,18 +2,16 @@ package main
 
 import "fmt"
 
-// 比較演算子
+// 論理演算子
 
 func main(){
-	fmt.Println(1==1)
+	fmt.Println(true && false == true)
+	fmt.Println(true && true == true)
+	fmt.Println(true && false == false)
 
-	fmt.Println(1==2)
+	fmt.Println(true || false == true)
+	fmt.Println(false || false == true)
 
-	fmt.Println(4<=8)
-
-	fmt.Println(1>9)
-
-	fmt.Println(true==false)
-
-	fmt.Println(true != false)
+	fmt.Println(!true)
+	fmt.Println(!false)
 }
