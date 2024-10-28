de8a210bfc6699028728c6c26844cac0f46ebaee
kpab <91237200+kpab@users.noreply.github.com>
2024-10-28 17:00:57 +0900

    Update main.go
    比較演算子

--- main.go
+++ main.go
@@ -2,34 +2,18 @@ package main
 
 import "fmt"
 
-// 算術演算子
-
-
-
+// 比較演算子
 
 func main(){
-	fmt.Println(1 + 2)
-	fmt.Println("ABC" + "DE") // 文字列の結合もできるよん
+	fmt.Println(1==1)
 
-	fmt.Println(5 - 1)
+	fmt.Println(1==2)
 
-	fmt.Println(5 * 4)
+	fmt.Println(4<=8)
 
-	fmt.Println(60 / 4)
+	fmt.Println(1>9)
 
-	fmt.Println(9 % 4) // 余り
+	fmt.Println(true==false)
 
-	// 算術演算子と代入
-	n := 0
-	n += 4
-	fmt.Println(n)
-	n++ // n = n + 1
-	fmt.Println(n)
-	n-- // n = n-1
-	fmt.Println(n)
-
-	s:="ABC"
-	s+="DE"
-	fmt.Println(s)
-
+	fmt.Println(true != false)
 }
