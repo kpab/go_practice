282e45ed3506305813fb0ea9520e6078dc797508
kpab <91237200+kpab@users.noreply.github.com>
2024-10-29 16:26:49 +0900

    Update main.go
    スライス
    append make len cap

--- main.go
+++ main.go
@@ -2,10 +2,41 @@ package main
 
 import "fmt"
 
+// append make len cap
+func main(){
+	sl := []int{100, 200}
+	fmt.Println(sl)
+	// append
+	sl = append(sl, 300)
+	fmt.Println(sl)
+
+	sl = append(sl, 400, 500, 600)
+	fmt.Println(sl)
+
+	// make
+	sl2 := make([]int, 5)
+	fmt.Println(sl2)
+	// len
+	fmt.Println(len(sl2))
+	// cap キャパシティー（容量）
+	fmt.Println(cap(sl2))
+
+	sl3 := make([]int, 5, 10)
+	fmt.Println(len(sl3))
+	fmt.Println(cap(sl3))
+
+	sl3 = append(sl3, 1, 2, 3, 4, 5, 6, 7)
+	fmt.Println(len(sl3))
+	fmt.Println(cap(sl3))
+}
+
+
+
+
 // スライス
 // 宣言, 操作
 // 配列と違い要素数指定しない
-
+/*
 func main(){
 	var sl []int
 	fmt.Println(sl)
@@ -41,3 +72,4 @@ func main(){
 	fmt.Println(sl5[1:len(sl5)-1])
 }
 
+*/
