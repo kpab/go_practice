f4e8668f7c0e21c406aa4e908a826e28b9b18e6a
kpab <91237200+kpab@users.noreply.github.com>
2024-10-29 21:28:59 +0900

    スライス可変長引数

--- main.go
+++ main.go
@@ -2,6 +2,28 @@ package main
 
 import "fmt"
 
+// スライス可変長引数
+func Sum(s ...int) int {
+	n := 0
+	for _, v := range(s){
+		n += v
+	}
+	return n
+}
+
+func main(){
+	fmt.Println(Sum(1, 2, 3))
+
+	fmt.Println(Sum(1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
+
+	fmt.Println(Sum())
+
+	sl := []int{1, 2, 3}
+	fmt.Println(Sum(sl...))
+}
+
+
+/*
 // スライス for
 func main(){
 	sl := []string{"A", "B", "C"}
@@ -9,15 +31,15 @@ func main(){
 	/*
 	for _, v := range sl{
 		fmt.Println(v)
-	}	
-	*/
-
+	}
+*/
+/*
 	// 古典的 for
 	for i:=0;i<len(sl);i++ {
-		fmt.Println(sl[i])	
+		fmt.Println(sl[i])
 	}
 }
-
+*/
 
 /*
 // コピー
