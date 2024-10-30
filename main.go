93b54a0e75f0fda67476196bbaaa54b98f5102eb
kpab <91237200+kpab@users.noreply.github.com>
2024-10-30 12:02:07 +0900

    チャネル基礎

--- main.go
+++ main.go
@@ -2,6 +2,56 @@ package main
 
 import "fmt"
 
+// チャネル: 複数のゴルーチン間でデータの受け渡しをするために設計されたデータ構造
+// 宣言・操作
+
+func main(){
+	var ch1 chan int // 送受信
+	
+	// var cha2 <- chan int // 受信専用
+	// var cha3 chan <- int // 送信専用
+
+	ch1 = make(chan int)
+
+	ch2 := make(chan int)
+
+	fmt.Println(cap(ch1), cap(ch2))
+
+	// バッファサイズを指定して宣言
+	ch3 := make(chan int, 5)
+	fmt.Println(cap(ch3))
+
+	// データをチャネルに送信
+	ch3 <- 3
+	fmt.Println(len(ch3))
+
+	ch3 <- 2
+	ch3 <- 1
+	fmt.Println(ch3)
+
+	i := <- ch3
+	fmt.Println(i)
+	fmt.Println("len", len(ch3))
+	i2 := <- ch3
+	fmt.Println(i2)
+	fmt.Println("len", len(ch3))
+
+	fmt.Println(<-ch3)
+	fmt.Println("len", len(ch2))
+
+	ch3 <- 1
+	ch3 <- 2
+	ch3 <- 3
+	ch3 <- 4
+	ch3 <- 5
+	fmt.Println(<-ch3)
+	ch3 <-6 // deadlock!
+} 
+
+
+
+
+/*
 // マップfor
 func main(){
 	m := map[string]int{
@@ -12,6 +62,7 @@ func main(){
 		fmt.Println(k, v)
 	}
 }
+*/
 
 /*
 // マップ(pythonの辞書型)
