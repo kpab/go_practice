d1e84da396596c2557cf2dfb8e9c171dc40865a6
kpab <91237200+kpab@users.noreply.github.com>
2024-11-06 17:01:20 +0900

    rand

--- main.go
+++ main.go
@@ -2,9 +2,37 @@ package main
 
 import (
 	"fmt"
-	"math"
+	"math/rand"
+	"time"
 )
 
+// rand
+func main() {
+	rand.Seed(256)
+
+	fmt.Println(rand.Float64())
+	fmt.Println(rand.Float64())
+	fmt.Println(rand.Float64())	
+	// 	0.813527291469711
+	// 0.5598026045235738
+	// 0.6695717783859498
+
+	//  現在の時刻をシードに使った擬似乱数の生成
+	fmt.Println(time.Now().UnixNano())
+	rand.Seed(time.Now().UnixNano())
+	// 0~99 の間の擬似乱数
+	fmt.Println(rand.Intn(100))
+	// int型の擬似乱数
+	fmt.Println(rand.Int())
+	// int32型の擬似乱数
+	fmt.Println(rand.Int31())
+	// int64型の擬似乱数
+	fmt.Println(rand.Int63())
+	// unit32型
+	fmt.Println(rand.Uint32())
+}
+
+/*
 // math
 func main() {
 	// 小数点以下の切り捨て切り上げ
