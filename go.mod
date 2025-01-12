c860a8641ab407cc7f7b13d7afe4fceb652938b6
w.kashiwagi <kpablow1122@gmail.com>
2025-01-12 17:41:11 +0900

    postgresql

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module go_practice
 
 go 1.23.2
 
-require github.com/mattn/go-sqlite3 v1.14.24 // indirect
+require (
+	github.com/lib/pq v1.10.9 // indirect
+	github.com/mattn/go-sqlite3 v1.14.24 // indirect
+)
--- main.go
+++ main.go
@@ -4,28 +4,88 @@ import (
 	"database/sql"
 	"log"
 
-	_ "github.com/mattn/go-sqlite3"
+	_ "github.com/lib/pq"
 )
 
 // database + sqlite3
 // テーブル作成
 var Db *sql.DB
 
+var err error
+
+type Person struct {
+	name string
+	age int
+}
+
 func main () {
-	Db, _ := sql.Open("sqlite3", "./example.sql")
+	Db, err = sql.Open("postgres", "user=test_user dbname=testdb password=password sslmode=disable")
+	if err != nil {
+		log.Println(err)
+	}
 	defer Db.Close()
 
-	// cmd := `CREATE TABLE IF NOT EXISTS persons(
-	// 			name STRING,
-	// 			age INT)`
-	
-	// データの追加
-	cmd := `INSERT INTO persons (name, age) VALUES (?, ?)`
-	_, err := Db.Exec(cmd, "tarou", 20)
+	/*
+	// C
+	cmd := `INSERT INTO persons (name, age) VALUES ($1, $2) `
+	_, err := Db.Exec(cmd, "Alex", 20)
+	if err != nil {
+		log.Fatalln(err)
+	}
+		*/
+	/*
+	// R
+	cmd := `SELECT * FROM persons WHERE age = $1 `
+	// QueryRow 1レコード取得
+	row := Db.QueryRow(cmd, 20)
+	var p Person
+	err = row.Scan(&p.name, &p.age)
+	if err != nil {
+		// データが無かったら
+		if err == sql.ErrNoRows {
+			log.Println("no row")
+		} else {
+			log.Println(err)
+		}
+	}
+	fmt.Println(p.name, p.age)
+
+	cmd = `SELECT * FROM persons`
+	// Queryは条件に合うものを全て取得
+	rows, _ := Db.Query(cmd)
+	defer rows.Close()
+	// structを作成
+	var pp []Person
+	// 取得したデータをループでスライスに追加 for rows.Next
+	for rows.Next() {
+		var p Person
+		// scan データ追加
+		err := rows.Scan(&p.name, &p.age)
+		// 1行ずつエラーハンドリングver
+		if err != nil {
+			log.Println(err)
+		}
+		pp = append(pp, p)
+	}
+	// まとめてエラーハンドリングver
+	err := rows.Err()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	// 表示
+	for _, p := range pp {
+		fmt.Println(p.name, p.age)
+	}
+	*/
+
+	// U
+	cmd := `UPDATE persons SET age = $1 WHERE name = $2`
+	_, err := Db.Query(cmd, 22, "Alex")
 	if err != nil {
 		log.Fatalln(err)
 	}
 
-	// データの更新：省略
+	// D 省略
+
 
 }
