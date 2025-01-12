185ba97ac7289ae2e1070da909ebbb69bca67f23
w.kashiwagi <kpablow1122@gmail.com>
2025-01-12 17:54:02 +0900

    ini

--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.23.2
 require (
 	github.com/lib/pq v1.10.9 // indirect
 	github.com/mattn/go-sqlite3 v1.14.24 // indirect
+	gopkg.in/go-ini/ini.v1 v1.67.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,91 +1,35 @@
 package main
 
 import (
-	"database/sql"
-	"log"
+	"fmt"
 
-	_ "github.com/lib/pq"
+	"gopkg.in/go-ini/ini.v1"
 )
 
-// database + sqlite3
-// テーブル作成
-var Db *sql.DB
+// ini
 
-var err error
-
-type Person struct {
-	name string
-	age int
+type ConfigList struct {
+	Port int
+	DbName string
+	SQLDriver string
 }
 
-func main () {
-	Db, err = sql.Open("postgres", "user=test_user dbname=testdb password=password sslmode=disable")
-	if err != nil {
-		log.Println(err)
-	}
-	defer Db.Close()
+var Config ConfigList
 
-	/*
-	// C
-	cmd := `INSERT INTO persons (name, age) VALUES ($1, $2) `
-	_, err := Db.Exec(cmd, "Alex", 20)
-	if err != nil {
-		log.Fatalln(err)
-	}
-		*/
-	/*
-	// R
-	cmd := `SELECT * FROM persons WHERE age = $1 `
-	// QueryRow 1レコード取得
-	row := Db.QueryRow(cmd, 20)
-	var p Person
-	err = row.Scan(&p.name, &p.age)
-	if err != nil {
-		// データが無かったら
-		if err == sql.ErrNoRows {
-			log.Println("no row")
-		} else {
-			log.Println(err)
-		}
-	}
-	fmt.Println(p.name, p.age)
+func init() {
+	cfg, _ := ini.Load("config.ini")
 
-	cmd = `SELECT * FROM persons`
-	// Queryは条件に合うものを全て取得
-	rows, _ := Db.Query(cmd)
-	defer rows.Close()
-	// structを作成
-	var pp []Person
-	// 取得したデータをループでスライスに追加 for rows.Next
-	for rows.Next() {
-		var p Person
-		// scan データ追加
-		err := rows.Scan(&p.name, &p.age)
-		// 1行ずつエラーハンドリングver
-		if err != nil {
-			log.Println(err)
-		}
-		pp = append(pp, p)
-	}
-	// まとめてエラーハンドリングver
-	err := rows.Err()
-	if err != nil {
-		log.Fatalln(err)
-	}
-	// 表示
-	for _, p := range pp {
-		fmt.Println(p.name, p.age)
-	}
-	*/
+	Config = ConfigList{
+		Port: cfg.Section("web").Key("port").MustInt(8080),
 
-	// U
-	cmd := `UPDATE persons SET age = $1 WHERE name = $2`
-	_, err := Db.Query(cmd, 22, "Alex")
-	if err != nil {
-		log.Fatalln(err)
+		DbName: cfg.Section("db").Key("name").MustString("example.sql"),
+
+		SQLDriver: cfg.Section("db").Key("driver").String(),
 	}
+}
 
-	// D 省略
-
-
+func main() {
+	fmt.Printf("Port = %v\n", Config.Port)
+	fmt.Printf("DbName = %v\n", Config.DbName)
+	fmt.Printf("SQLDriver = %v\n", Config.SQLDriver)
 }
