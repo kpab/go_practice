0dac3855efd823f27a3c2fabc16dae1e7586c87f
w.kashiwagi <kpablow1122@gmail.com>
2025-01-12 15:49:13 +0900

    net/url

--- main.go
+++ main.go
@@ -1,47 +1,60 @@
 package main
 
 import (
-	"context"
 	"fmt"
-	"time"
+	"net/url"
 )
 
+// net/url
+func main () {
+	// URLを解析
+	u, _ := url.Parse("http://example.com/search?a=1&b=2#top")
+	fmt.Println(u.Scheme)
+	fmt.Println(u.Host)
+	fmt.Println(u.Path)
+	fmt.Println(u.RawQuery)
+	fmt.Println(u.Fragment)
+
+	fmt.Println(u.Query())
+
+	//URLを生成
+	// 省略
+}
 // context
-func longProcess(ctx context.Context, ch chan string)  {
-	fmt.Println("開始")
-	time.Sleep(2 * time.Second)
-	fmt.Println("終了")
-	ch <- "実行結果"
-}
+// func longProcess(ctx context.Context, ch chan string)  {
+// 	fmt.Println("開始")
+// 	time.Sleep(2 * time.Second)
+// 	fmt.Println("終了")
+// 	ch <- "実行結果"
+// }
 
-func main() {
-	ch := make(chan string)
+// func main() {
+// 	ch := make(chan string)
 
-	ctx := context.Background()
+// 	ctx := context.Background()
 
-	ctx, cancel := context.WithTimeout(ctx, 1*time.Second)
+// 	ctx, cancel := context.WithTimeout(ctx, 1*time.Second)
 
-	defer cancel()
+// 	defer cancel()
 
-	go longProcess(ctx, ch)
+// 	go longProcess(ctx, ch)
 
-L:
-	for {
-		select {
-		case <- ctx.Done():
-			fmt.Println("#####Error######")
-			fmt.Println(ctx.Err())
-			break L
-		case s := <-ch:
-			fmt.Println(s)
-			fmt.Println("success")
-			break L
-		}
-	}
+// L:
+// 	for {
+// 		select {
+// 		case <- ctx.Done():
+// 			fmt.Println("#####Error######")
+// 			fmt.Println(ctx.Err())
+// 			break L
+// 		case s := <-ch:
+// 			fmt.Println(s)
+// 			fmt.Println("success")
+// 			break L
+// 		}
+// 	}
 
-	fmt.Println("ループ抜けたm ")
-}
-
+// 	fmt.Println("ループ抜けたm ")
+// }
 
 // sort
 
